b24017d80c5112c2c76d83808715ac532352c0d2
Adam Birgenheier <adamcarlbirgenheier@gmail.com>
2020-08-15 16:41:49 -0700

    Updates

---
+++ precache-manifest.a6ba24078f55893b2c1b866947c124de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "beb5558de0800a214e7d377863719f4d",
+    "url": "/ETracker/index.html"
+  },
+  {
+    "revision": "cc1d10c20554a5b1f076",
+    "url": "/ETracker/static/css/main.6d64acd2.chunk.css"
+  },
+  {
+    "revision": "bf8d17cfbc58a9850d1c",
+    "url": "/ETracker/static/js/2.d0b8e89b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ETracker/static/js/2.d0b8e89b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc1d10c20554a5b1f076",
+    "url": "/ETracker/static/js/main.50e8062b.chunk.js"
+  },
+  {
+    "revision": "91fe78c4f01d844b1dbb",
+    "url": "/ETracker/static/js/runtime-main.fa3a6d78.js"
+  }
+]);
